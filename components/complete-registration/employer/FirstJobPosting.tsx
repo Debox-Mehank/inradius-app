045152d046391c116cf6004280a450630b06e6d0
Yash <yash@Yashs-MacBook-Air.local>
2022-05-14 00:27:22 -0400

    Added Confetti animation

--- components/complete-registration/employer/FirstJobPosting.tsx
+++ components/complete-registration/employer/FirstJobPosting.tsx
@@ -1,18 +1,21 @@
 import Link from 'next/link'
-import React from 'react'
+import React, { useState } from 'react'
 import { useDispatch, useSelector } from "react-redux";
 import { incrementProgress, incrementStep } from "../../../features/companyRegistrationSlice";
 import { useRouter } from "next/router";
 import { RootState } from "../../../app/store";
 import { Dispatch, SetStateAction } from "react";
-
+import Confetti from 'react-confetti'
 
 function FirstJobPosting() {
     const router = useRouter()
     const registration = useSelector((state: RootState) => state.companyRegistration)
     const dispatch = useDispatch();
+    const [rec, setRec] = useState(true)
+    setTimeout(() => setRec(false), 5000)
   return (
     <div className='flex flex-col justify-center items-center h-full font-semibold text-center'>
+    <Confetti width={1200} height={780} numberOfPieces={600} recycle={rec} gravity={0.15}/>
     Congratulations on being verified! <br />We are delighted that you choose inRadius to help you in your employee hunting process.
     <div className="cursor-pointer text-primary inline-block flex flex-row"> <div onClick={() => {
                     const nextFunc = () => {

183468da165278281f665c279189c7b735ad8c72
Debox-Mehank <mehank@debox.co.in>
2022-06-10 16:30:12 +0530

    logo changes

--- components/reusables/Logo.tsx
+++ components/reusables/Logo.tsx
@@ -6,8 +6,7 @@ const Logo = () => {
       id="Layer_2"
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 400.21 251.53"
-      width={400}
-      height={400}
+      width={500}
     >
       <defs></defs>
       <g id="Layer_1-2">
